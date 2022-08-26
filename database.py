128f022d4ce3ed1383acd3bef7c0d924cc93f7d8
Deep Sarker <deepsarker01@gmail.com>
2022-08-26 23:44:58 +0600

    delete all files to upload program with new structure

--- database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-SQL_ALCHEMY_URL = "sqlite:///sqlalchemy.sqlite.todo_info"
-engine = create_engine(SQL_ALCHEMY_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
